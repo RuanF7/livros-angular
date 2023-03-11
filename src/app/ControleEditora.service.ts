13a3a813b3f6f18c2dd0e7f58c3850f253e5325f
Ruan Finamor <ruanfinamor@gmail.com>
2023-03-11 12:26:21 -0300

    Commit Inicial

---
+++ src/app/ControleEditora.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Editora } from './editora';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ControleEditoraService {
+
+
+  
+  constructor() { }
+}
